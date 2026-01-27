926567b04a787c010c9f5c7cc44648f251b564e4
Saptarshi <ssengupta8@gmail.com>
2026-01-27 14:24:48 -0500

    predictions calculate code

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -7,19 +7,20 @@ def main(prediction_dataset_path: str) -> None:
     def clean_prediction(text: str) -> str:
         match = re.search(r"<\/think>([\s\S]*)", text)
         answer = match.group(1) if match else None
-        if answer:
+        try:
             return answer.split('Answer:')[1].strip()
-        else:
-            return ""
+        except:
+            print(text)
 
     squad_metric = load("squad")
     prediction_dataset = load_from_disk(prediction_dataset_path)
     predictions, references = [], []
     for row in tqdm(prediction_dataset):
-        if clean_prediction(row['raw_responses']) == "":
-            print(row['raw_responses'], clean_prediction(row['raw_responses']), "\n.........")
-        predictions.append({'prediction_text': clean_prediction(row['raw_responses']), 'id': row['id']})
-        references.append([{'answers': {'answer_start': [0], 'text': [row['answer']]}, 'id': row['id']}])
+        clean_prediction(row['raw_responses'])
+        #if clean_prediction(row['raw_responses']) == "":
+        #    print(row['raw_responses'], clean_prediction(row['raw_responses']), "\n.........")
+        #predictions.append({'prediction_text': clean_prediction(row['raw_responses']), 'id': row['id']})
+        #references.append([{'answers': {'answer_start': [0], 'text': [row['answer']]}, 'id': row['id']}])
 
     '''
     results = squad_metric.compute(predictions=predictions, references=references)
