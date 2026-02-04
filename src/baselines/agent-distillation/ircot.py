20db1a59ff513686effb4ec0eaa245711940821f
Saptarshi <ssengupta8@gmail.com>
2026-02-04 12:59:19 -0500

    removed all of agent-distillation code - trying to implement IRCoT with their model

--- src/baselines/agent-distillation/ircot.py
+++ src/baselines/agent-distillation/ircot.py
@@ -86,7 +86,7 @@ def main():
         print(f"Working on {dataset}...")
         dataset = load_dataset('json',
                                data_files=f"../../../data/{dataset}/test.json",
-                               split='train')
+                               split='train').select(range(5))
 
         all_questions = [row['question'] for row in dataset]
         dataset_responses = run_ircot(all_questions, wiki_corpus, index, embedding_model, slm, tokenizer)
