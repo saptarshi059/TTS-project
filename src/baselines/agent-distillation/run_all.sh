eb4c43d92f1bc74f1ab37f2f34388f6c622cfbef
Saptarshi <ssengupta8@gmail.com>
2026-02-05 17:49:20 -0500

    Fresh sampled data

--- src/baselines/agent-distillation/run_all.sh
+++ src/baselines/agent-distillation/run_all.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#This tells the shell to stop immediately if any command returns an error.
-#Without it, if the first script crashes 5 minutes after you go to bed, the second one will still run.
-set -e
 
-python ircot_frames.py
-python ircot_three_ds.py
+python -u ircot_frames.py
+python -u ircot_three_ds.py
