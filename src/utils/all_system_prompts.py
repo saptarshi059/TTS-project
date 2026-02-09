b9c6c41e4d947469608900925add6f495342cc49
Saptarshi <ssengupta8@gmail.com>
2026-02-09 14:12:13 -0500

    Hypergraph RAG

--- src/utils/all_system_prompts.py
+++ src/utils/all_system_prompts.py
@@ -1,6 +1,6 @@
 NAIVE_BASELINE = ("Answer the given question using the provided knowledge graph triples. "
                   "Please keep your response concise. If no answer exists, please return ''. "
-                  "Format your response as\nAnswer: <answer text>")
+                  "Format your response as\nAnswer: <answer text> ")
 
 SYSTEM_1 = ("Answer the given question without writing any additional text. "
             "Format your response as\nAnswer: <answer text>")
