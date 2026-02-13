7c81ee08427bda71eeac5d22bd3eb94cb5f5f040
Saptarshi <ssengupta8@gmail.com>
2026-02-13 17:21:10 -0500

    GraphAnchor - fixing file paths for index.py

--- src/baselines/GraphAnchor-main/src/run_tests_GA.sh
+++ src/baselines/GraphAnchor-main/src/run_tests_GA.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build index
-python -u build_index/emb/index.py --dataset 2wikimultihopqa --model bge-large-en-v1.5
-python -u build_index/emb/index.py --dataset hotpotqa --model bge-large-en-v1.5
-python -u build_index/emb/index.py --dataset musique --model bge-large-en-v1.5
-python -u build_index/emb/index.py --dataset frames --model bge-large-en-v1.5
+python -u build_index/emb/index.py --dataset 2wikimultihopqa --model qwen3-Embedding-0.6B
+python -u build_index/emb/index.py --dataset hotpotqa --model qwen3-Embedding-0.6B
+python -u build_index/emb/index.py --dataset musique --model qwen3-Embedding-0.6B
+python -u build_index/emb/index.py --dataset frames --model qwen3-Embedding-0.6B
 
 # Run the actual tests
 CUDA_VISIBLE_DEVICES=2 python -u GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset 2wikimultihopqa --max_step 3 --model qwen2.5-7b-instruct
