2e4b8d57a4042128f129de4a7032d9541f2b7a11
Saptarshi <ssengupta8@gmail.com>
2026-02-13 14:27:21 -0500

    GraphAnchor - finally everything works. Letting it run now.

--- src/baselines/GraphAnchor-main/src/run_tests_GA.sh
+++ src/baselines/GraphAnchor-main/src/run_tests_GA.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build index
-python build_index/emb/index.py --dataset 2wikimultihopqa --model bge-base-en-v1.5
-python build_index/emb/index.py --dataset hotpotqa --model bge-base-en-v1.5
-python build_index/emb/index.py --dataset musique --model bge-base-en-v1.5
-python build_index/emb/index.py --dataset frames --model bge-base-en-v1.5
+python -u build_index/emb/index.py --dataset 2wikimultihopqa --model bge-base-en-v1.5
+python -u build_index/emb/index.py --dataset hotpotqa --model bge-base-en-v1.5
+python -u build_index/emb/index.py --dataset musique --model bge-base-en-v1.5
+python -u build_index/emb/index.py --dataset frames --model bge-base-en-v1.5
 
 # Run the actual tests
-CUDA_VISIBLE_DEVICES=2 python GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset 2wikimultihopqa --max_step 3 --model qwen2.5-7b-instruct
-CUDA_VISIBLE_DEVICES=2 python GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset hotpotqa --max_step 3 --model qwen2.5-7b-instruct
-CUDA_VISIBLE_DEVICES=2 python GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset musique --max_step 3 --model qwen2.5-7b-instruct
-CUDA_VISIBLE_DEVICES=2 python GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset frames --max_step 3 --model qwen2.5-7b-instruct
+CUDA_VISIBLE_DEVICES=2 python -u GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset 2wikimultihopqa --max_step 3 --model qwen2.5-7b-instruct
+CUDA_VISIBLE_DEVICES=2 python -u GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset hotpotqa --max_step 3 --model qwen2.5-7b-instruct
+CUDA_VISIBLE_DEVICES=2 python -u GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset musique --max_step 3 --model qwen2.5-7b-instruct
+CUDA_VISIBLE_DEVICES=2 python -u GraphAnchor.py --method GraphAnchor --retrieve_top_k 5 --dataset frames --max_step 3 --model qwen2.5-7b-instruct
