4a56d5450f7bc1141b0e44c5895904b7fd64a9ad
Saptarshi <ssengupta8@gmail.com>
2026-02-18 10:27:47 -0500

    agent-distillation retry

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -1,23 +1,17 @@
-from datasets import load_from_disk, tqdm
 from argparse import ArgumentParser
+
+from datasets import load_dataset, tqdm
 from evaluate import load
-import re
 
-def main(prediction_dataset_path: str) -> None:
-    def clean_prediction(text: str) -> str:
-        match = re.search(r"<\/think>([\s\S]*)", text)
-        answer = match.group(1) if match else None
-        try:
-            return answer.split('Answer:')[1].strip()
-        except:
-            return ""
 
-    squad_metric = load("squad")
-    prediction_dataset = load_from_disk(prediction_dataset_path)
+def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth_field: str) -> None:
+
+    squad_metric = load("squad_v2")
+    prediction_dataset = load_dataset('json', data_files=prediction_dataset_path)
     predictions, references = [], []
-    for row in tqdm(prediction_dataset):
-        predictions.append({'prediction_text': clean_prediction(row['raw_responses']), 'id': row['id']})
-        references.append({'answers': {'answer_start': [0], 'text': [row['answer']]}, 'id': row['id']})
+    for idx, row in tqdm(enumerate(prediction_dataset)):
+        predictions.append({'prediction_text': row[predicted_answer_field], 'id': row[idx]})
+        references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': row[idx]})
 
     results = squad_metric.compute(predictions=predictions, references=references)
     print(results)
@@ -25,5 +19,9 @@ def main(prediction_dataset_path: str) -> None:
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--prediction_dataset_path", type=str)
+    parser.add_argument("--predicted_answer_field", type=str)
+    parser.add_argument("--ground_truth_field", type=str)
     args = parser.parse_args()
-    main(prediction_dataset_path=args.prediction_dataset_path)
+    main(prediction_dataset_path=args.prediction_dataset_path,
+         predicted_answer_field=args.predicted_answer_field,
+         ground_truth_field=args.ground_truth_field)
