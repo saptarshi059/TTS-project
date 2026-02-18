881c2c7dce51ee7fc1f6c3f5e6f08321474c275a
Saptarshi <ssengupta8@gmail.com>
2026-02-18 10:34:49 -0500

    agent-distillation retry

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -10,8 +10,8 @@ def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth
     prediction_dataset = load_dataset('json', data_files=prediction_dataset_path, split='train')
     predictions, references = [], []
     for idx, row in tqdm(enumerate(prediction_dataset)):
-        predictions.append({'prediction_text': row[predicted_answer_field], 'id': row[str(idx)]})
-        references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': row[str(idx)]})
+        predictions.append({'prediction_text': row[predicted_answer_field], 'id': str(idx)})
+        references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
 
     results = squad_metric.compute(predictions=predictions, references=references)
     print(results)
