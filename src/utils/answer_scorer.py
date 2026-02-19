f65c885ebe9a30b106ee73355fe67527a2bf8f62
Saptarshi <ssengupta8@gmail.com>
2026-02-18 23:51:29 -0500

    agent-distillation retry

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -6,12 +6,12 @@ from evaluate import load
 
 def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth_field: str) -> None:
 
-    squad_metric = load("squad")
+    squad_metric = load("squad_v2")
     prediction_dataset = load_dataset('json', data_files=prediction_dataset_path, split='train')
     predictions, references = [], []
     for idx, row in tqdm(enumerate(prediction_dataset)):
         pred = "" if row[predicted_answer_field] is None else row[predicted_answer_field]
-        predictions.append({'prediction_text': pred, 'id': str(idx)})
+        predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
         references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
 
     results = squad_metric.compute(predictions=predictions, references=references)
