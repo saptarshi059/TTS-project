21fc817af4e018338b5cad41c040973ae9f2b886
Saptarshi <ssengupta8@gmail.com>
2026-02-20 12:13:44 -0500

    agent-distillation retry

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -2,17 +2,27 @@ from argparse import ArgumentParser
 
 from datasets import load_dataset, tqdm
 from evaluate import load
+import pandas as pd
 
 
 def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth_field: str) -> None:
 
     squad_metric = load("squad_v2")
-    prediction_dataset = load_dataset('json', data_files=prediction_dataset_path, split='train')
     predictions, references = [], []
-    for idx, row in tqdm(enumerate(prediction_dataset)):
-        pred = "" if row[predicted_answer_field] is None else row[predicted_answer_field]
-        predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
-        references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
+
+    try:
+        prediction_dataset = load_dataset('json', data_files=prediction_dataset_path, split='train')
+        for idx, row in tqdm(enumerate(prediction_dataset)):
+            pred = "" if row[predicted_answer_field] is None else row[predicted_answer_field]
+            predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
+            references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
+    except:
+        prediction_dataset = pd.read_json(prediction_dataset_path, lines=True)
+        for idx, row in tqdm(enumerate(prediction_dataset.itertuples())):
+            pred = "" if row[predicted_answer_field] is None else row[predicted_answer_field]
+            predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
+            references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
+
 
     results = squad_metric.compute(predictions=predictions, references=references)
     print(results)
