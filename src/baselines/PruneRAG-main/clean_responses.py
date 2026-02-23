427429767bace5de7fca058e7e1d2d6aea1d7687
Saptarshi <ssengupta8@gmail.com>
2026-02-23 12:11:08 -0500

    PruneRAG - cleaning responses

---
+++ src/baselines/PruneRAG-main/clean_responses.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+import pandas as pd
+
+def main(dataset_path):
+    ds = pd.read_json(dataset_path, lines=True)
+    for row in ds.itertuples():
+        if '\text{' in row.extracted_answer:
+            row.extracted_answer = row.extracted_answer.rstrip('\text{')
+        elif '\\text{' in row.extracted_answer:
+            row.extracted_answer = row.extracted_answer.rstrip('\\text{')
+
+    ds.to_json(dataset_path, index=False)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--responses_file", type=str)
+    args = parser.parse_args()
+    main(dataset_path=args.response_file)
