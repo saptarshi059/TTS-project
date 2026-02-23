6b051af437f51c4f04f149684a72f69ebc821bc9
Saptarshi <ssengupta8@gmail.com>
2026-02-23 13:01:18 -0500

    PruneRAG - cleaning responses

--- src/baselines/PruneRAG-main/clean_and_format_responses.py
+++ src/baselines/PruneRAG-main/clean_and_format_responses.py
@@ -3,13 +3,20 @@ import pandas as pd
 import re
 
 def main(dataset_path):
-    df = pd.read_json(dataset_path, lines=True)
-
     def clean_latex_text(text):
         return re.sub(r'\\+text\{', '', text).strip()
 
+    df = pd.read_json(dataset_path, lines=True)
     df['extracted_answer'] = df['extracted_answer'].apply(clean_latex_text)
 
+    final_answers = []
+    for row in df.itertuples():
+        if row.extracted_answer == '':
+            final_answers.append(row.extracted_answer)
+        else:
+            final_answers.append(row.full_response)
+
+    df['final_answer'] = final_answers
     df.to_json(dataset_path, orient='records', lines=True)
 
 if __name__ == "__main__":
