bf17cae7741f38b9ebe6f80b54ab19cb1289957e
Saptarshi <ssengupta8@gmail.com>
2026-02-23 20:18:49 -0500

    PruneRAG -

--- src/baselines/PruneRAG-main/generate_combined_results.py
+++ src/baselines/PruneRAG-main/generate_combined_results.py
@@ -6,7 +6,7 @@ def main(dataset):
     original_ds = pd.read_json(f"../../../sampled_data/{dataset}/sampled_ds.json")
     generated_ds = pd.read_json(f'outputs/{dataset}_outputs.jsonl', lines=True)
     combined_df = pd.merge(original_ds, generated_ds).drop(columns=['id', '_1', 'type', 'context', 'entity_ids',
-       'supporting_facts', 'evidences', 'evidences_id', 'answer_id', 'request_id'], inplace=True)
+       'supporting_facts', 'evidences', 'evidences_id', 'answer_id', 'request_id'])
 
     combined_df.to_json(f"outputs/{dataset}_parsed.jsonl", orient='records', lines=True)
 
