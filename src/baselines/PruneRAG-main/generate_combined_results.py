390fec76815f84b818e6857cad267037ad9d0d8a
Saptarshi <ssengupta8@gmail.com>
2026-02-23 20:17:25 -0500

    PruneRAG -

--- src/baselines/PruneRAG-main/generate_combined_results.py
+++ src/baselines/PruneRAG-main/generate_combined_results.py
@@ -1,14 +1,14 @@
 from argparse import ArgumentParser
-from datasets import Dataset
 import pandas as pd
 
 
 def main(dataset):
     original_ds = pd.read_json(f"../../../sampled_data/{dataset}/sampled_ds.json")
     generated_ds = pd.read_json(f'outputs/{dataset}_outputs.jsonl', lines=True)
-    combined_df = pd.merge(original_ds, generated_ds)
-    combined_df = Dataset.from_pandas(combined_df)
-    combined_df.to_json(f"outputs/{dataset}_parsed.jsonl")
+    combined_df = pd.merge(original_ds, generated_ds).drop(columns=['id', '_1', 'type', 'context', 'entity_ids',
+       'supporting_facts', 'evidences', 'evidences_id', 'answer_id', 'request_id'], inplace=True)
+
+    combined_df.to_json(f"outputs/{dataset}_parsed.jsonl", orient='records', lines=True)
 
 
 if __name__ == "__main__":
