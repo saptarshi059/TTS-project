b8db907bcc25a35b9ed4c2ea259b8e3cf3be93c4
Saptarshi <ssengupta8@gmail.com>
2026-03-01 18:53:24 -0500

    RT-RAG: getting the actual retrieved document needed a bit of a fix since I'm not using reranking.

--- src/baselines/RT-RAG-main/main/run_all.sh
+++ src/baselines/RT-RAG-main/main/run_all.sh
@@ -2,7 +2,14 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
+echo "-----------------Working on 2wikimultihopqa-----------------"
 ./run_dataset 2wikimultihopqa
+
+echo "-----------------Working on hotpotqa-----------------"
 ./run_dataset hotpotqa
+
+echo "-----------------Working on musique-----------------"
 ./run_dataset musique
+
+echo "-----------------Working on frames-----------------"
 ./run_dataset frames
