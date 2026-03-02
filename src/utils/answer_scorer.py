6e286aab14fc8390dd1c13f2f6e4c9726507973c
Saptarshi <ssengupta8@gmail.com>
2026-03-02 11:21:36 -0500

    RT-RAG: create_parsed_response_files.py for responses

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 
 def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth_field: str) -> None:
-
     squad_metric = load("squad_v2")
     predictions, references = [], []
 
@@ -15,13 +14,13 @@ def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth
         for idx, row in tqdm(enumerate(prediction_dataset)):
             pred = "" if row[predicted_answer_field] is None else row[predicted_answer_field]
             predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
-            references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
+            references.append({'answers': {'answer_start': [0], 'text': [*row[ground_truth_field]]}, 'id': str(idx)})
     except:
         prediction_dataset = pd.read_json(prediction_dataset_path, lines=True)
         for idx, row in tqdm(enumerate(prediction_dataset.itertuples())):
             pred = "" if getattr(row, predicted_answer_field) is None else getattr(row, predicted_answer_field)
             predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
-            references.append({'answers': {'answer_start': [0], 'text': [getattr(row, ground_truth_field)]}, 'id': str(idx)})
+            references.append({'answers': {'answer_start': [0], 'text': [*getattr(row, ground_truth_field)]}, 'id': str(idx)})
 
     results = squad_metric.compute(predictions=predictions, references=references)
     print(results)
