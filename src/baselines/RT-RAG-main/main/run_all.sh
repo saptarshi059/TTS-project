22689bda80440b4b0c7fafb2eb29239c74aab027
Saptarshi <ssengupta8@gmail.com>
2026-03-02 12:25:03 -0500

    RT-RAG: changed config.py for FRAMES - otherwise it crashed

--- src/baselines/RT-RAG-main/main/run_all.sh
+++ src/baselines/RT-RAG-main/main/run_all.sh
@@ -2,14 +2,14 @@
 
 set -e # Exit immediately if a command exits with a non-zero status
 
-echo "-----------------Working on 2wikimultihopqa-----------------"
-./run_dataset.sh 2wikimultihopqa
+#echo "-----------------Working on 2wikimultihopqa-----------------"
+#./run_dataset.sh 2wikimultihopqa
 
-echo "-----------------Working on hotpotqa-----------------"
-./run_dataset.sh hotpotqa
+#echo "-----------------Working on hotpotqa-----------------"
+#./run_dataset.sh hotpotqa
 
-echo "-----------------Working on musique-----------------"
-./run_dataset.sh musique
+#echo "-----------------Working on musique-----------------"
+#./run_dataset.sh musique
 
 echo "-----------------Working on frames-----------------"
 ./run_dataset.sh frames
