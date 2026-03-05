af825fc41d6f60513a2acaeb36ed3c2384a8e954
Saptarshi <ssengupta8@gmail.com>
2026-03-05 17:22:34 -0500

    prepare_dataset_for_system_2.py now uses exact match from squad metric for filtration.

--- src/framework/system-1/prepare_dataset_for_system_2.py
+++ src/framework/system-1/prepare_dataset_for_system_2.py
@@ -11,9 +11,8 @@ def main(dataset: str):
     ds = pd.read_json(base_path / "parsed_responses.jsonl", lines=True)
     completed = []
     for idx, row in enumerate(ds.itertuples()):
-        print(row.cleaned_ans, row.answer)
-        prediction = {'prediction_text': row.cleaned_ans, 'id': str(idx), 'no_answer_probability': 0.}
-        reference = {'answers': {'answer_start': [0], 'text': [row.answer]}, 'id': str(idx)}
+        prediction = [{'prediction_text': row.cleaned_ans, 'id': str(idx), 'no_answer_probability': 0.}]
+        reference = [{'answers': {'answer_start': [0], 'text': [row.answer]}, 'id': str(idx)}]
 
         score = squad_metric.compute(predictions=prediction, references=reference)
         if score['exact'] == 100.0:
