c5d1c7f4e41f257c3675f33746f301ca453b5d92
Saptarshi <ssengupta8@gmail.com>
2026-03-06 14:37:06 -0500

    system-1: determinism

--- src/framework/system-1/run_all_system_1.sh
+++ src/framework/system-1/run_all_system_1.sh
@@ -7,4 +7,5 @@ for ds in "${DATASETS[@]}";do
   python step1.py --dataset "$ds"
   python parse_system1_raw_response.py --dataset "$ds"
   python prepare_dataset_for_system_2.py --dataset "$ds"
+  python answer_scorer.py --prediction_dataset_path "../../../framework_output/system1/${ds}/parsed_responses.jsonl" --predicted_answer_field "cleaned_ans" --ground_truth_field "answer"
 done
