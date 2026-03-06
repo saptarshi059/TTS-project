54cc9875c3fc2f65af2e4a334f3fc3e39b1f99fa
Saptarshi <ssengupta8@gmail.com>
2026-03-06 14:40:15 -0500

    system-1: determinism

--- src/framework/system-2/run_all_triple_gen.sh
+++ src/framework/system-2/run_all_triple_gen.sh
@@ -3,6 +3,6 @@
 DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
 
 for ds in "${DATASETS[@]}";do
-  echo "-------------Running system-1 for ${ds}-------------"
+  echo "-------------${ds}-------------"
   python triple_gen.py --dataset "$ds"
 done
