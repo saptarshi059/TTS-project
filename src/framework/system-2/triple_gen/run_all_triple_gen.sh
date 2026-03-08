f0a812ca18f887087b995db0ced8ef7db1e83cb7
Saptarshi <ssengupta8@gmail.com>
2026-03-07 20:38:45 -0500

    framework: parsing triple_gen.py

--- src/framework/system-2/triple_gen/run_all_triple_gen.sh
+++ src/framework/system-2/triple_gen/run_all_triple_gen.sh
@@ -5,4 +5,5 @@ DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
 for ds in "${DATASETS[@]}";do
   echo "-------------${ds}-------------"
   python triple_gen.py --dataset "$ds"
+  python parse_raw_responses.py --dataset "$ds"
 done
