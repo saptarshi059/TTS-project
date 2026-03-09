b97bc72c5d2fd843aa52b44449672d3bc9d936e9
Saptarshi <ssengupta8@gmail.com>
2026-03-09 19:43:50 -0400

    framework: generate_with_all_evidence.py - adding resume logic - not working otherwise

--- src/framework/system-2/final_generation/generate_with_all_evidence.py
+++ src/framework/system-2/final_generation/generate_with_all_evidence.py
@@ -1,21 +1,25 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed, DataCollatorWithPadding
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
 from torch.utils.data import Dataset, DataLoader
 from argparse import ArgumentParser
 from pathlib import Path
 from tqdm import tqdm
 import pandas as pd
 import os, torch
-import json
 import sys
 sys.path.append("../../../utils/")
 
 from all_system_prompts import SYSTEM_2_MAIN_PROMPT
+import re
+import json
+
 
 class System2Dataset(Dataset):
-    def __init__(self, tokenizer, dataset):
+    def __init__(self, tokenizer, dataset, device):
         self.tokenizer = tokenizer
+        self.dataset = dataset
+        self.device = device
         self.samples = []
-        for row in dataset.itertuples():
+        for row in tqdm(dataset.itertuples()):
             generated_triples_string = ", ".join(f"({triple})" for triple in row.generated_triples)
             retrieved_evidences = "\n\n".join(row.retrieved_docs)
 
@@ -26,50 +30,56 @@ class System2Dataset(Dataset):
                                                              f"Initial (incorrect) Reasoning (triples): {generated_triples_string}"
                                                              f"Counterfactual Evidence: {retrieved_evidences}"
                                                              f"</input>"}])
+        self.tokenized_samples = tokenizer.apply_chat_template(self.samples, tokenize=False, add_generation_prompt=True)
+        self.model_inputs = self.tokenizer(self.tokenized_samples, padding=True, return_tensors="pt")
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        formatted_ip = self.tokenizer.apply_chat_template(self.samples[idx], tokenize=False, add_generation_prompt=True)
-        inputs = self.tokenizer(formatted_ip, padding=False, return_tensors="pt")
-
         return {
-            "input_ids": inputs["input_ids"].squeeze(0),
-            "attention_mask": inputs["attention_mask"].squeeze(0),
+            "input_ids": self.model_inputs["input_ids"][idx].to(self.device),
+            "attention_mask": self.model_inputs["attention_mask"][idx].to(self.device),
         }
+
 
 def main(model_name:str, dataset:str, batch_size: int, gpu_id: str, output_dir: str) -> None:
     set_seed(42)
     os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
 
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name, padding_side='left', use_fast=False)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name, padding_side='left')
     model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=model_name,
                                                  dtype="auto",
                                                  attn_implementation="flash_attention_2",
                                                  device_map="auto")
 
     main_dataset = pd.read_json(f"../../../../framework_output/system2/{dataset}/retrieval_results/retrieved_docs.jsonl", lines=True)
-    print(f"Wrapping {dataset} with torch...")
-    collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    torch_dataset = System2Dataset(tokenizer=tokenizer, dataset=main_dataset)
-    torch_dataset_dataloader = DataLoader(torch_dataset, num_workers=0, batch_size=batch_size, collate_fn=collator)
-
-    print(f"{'-'*10}Running System-2: MAIN GENERATION with {model_name} on {dataset}{'-'*10}")
     op_dir = Path(output_dir) / f"{dataset}/final_response/"
     folder = Path(op_dir)
     folder.mkdir(parents=True, exist_ok=True)
-    with Path(op_dir / "streamed_raw_responses.jsonl").open("a") as file:
-        for batch in tqdm(torch_dataset_dataloader):
-            batch = {k: v.to(model.device) for k, v in batch.items()}
-            with torch.no_grad():
-                input_length = batch["input_ids"].shape[1]
-                generated_ids = model.generate(**batch, max_new_tokens=1024, top_p=0.8, temperature=0.7)
-                new_tokens = generated_ids[:, input_length:]  # slice off prompt
-                decoded_response = [tokenizer.decode(ids, skip_special_tokens=True) for ids in new_tokens]
 
-                for text in decoded_response:
-                    file.write(json.dumps({"response": text}) + "\n")
+    partial_op_file = Path(op_dir / "streamed_responses.jsonl")
+    if partial_op_file.exists():
+        completed_questions = pd.read_json(partial_op_file, lines=True)['question'].to_list()
+        main_dataset = main_dataset.query("question not in @completed_questions")
+        print(f"Resuming after completing {len(main_dataset)} questions...")
+
+    print(f"Wrapping {dataset} with torch...")
+    torch_dataset = System2Dataset(tokenizer=tokenizer, dataset=main_dataset, device=model.device)
+    torch_dataset_dataloader = DataLoader(torch_dataset, batch_size=batch_size, shuffle=False)
+
+    print(f"{'-'*10}Running System-2: MAIN GENERATION with {model_name} on {dataset}{'-'*10}")
+    with Path(op_dir / "streamed_responses.jsonl").open("a") as file:
+        for batch in tqdm(torch_dataset_dataloader):
+            with torch.no_grad():
+                generated_ids = model.generate(**batch, max_new_tokens=1024, top_p=0.8, temperature=0.7)
+                decoded_generation = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+                for generation in decoded_generation:
+                    ques = re.search(r"Question:(.*)", generation.split(SYSTEM_2_MAIN_PROMPT)[1])[1].strip()
+                    write_obj = {'question': ques, 'generation': generation}
+                    json_string = json.dumps(write_obj)
+                    file.write(json_string + '\n')
 
 
 if __name__ == "__main__":
