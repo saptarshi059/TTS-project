0c489bdc15a5df0950ab80721f21dbf81a97abde
Saptarshi <ssengupta8@gmail.com>
2026-03-10 14:07:03 -0400

    framework: system-2 - prompt change because maybe qwen2.5 can't really reason that well

--- src/framework/system-2/final_generation/run_all_final_gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
-
-for ds in "${DATASETS[@]}";do
-  echo "-------------${ds}-------------"
-  python -u generate_with_all_evidence.py --dataset "$ds"
-done
