7a0aa7d7c5e748ea33f916db4d34e0548e324bae
Saptarshi <ssengupta8@gmail.com>
2026-03-11 12:38:29 -0400

    framework: system-2 - updated parsing to handle fail cases

--- src/framework/system-2/final_generation/keep_running.sh
+++ src/framework/system-2/final_generation/keep_running.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-until python generate_with_all_evidence.py --dataset "frames"; do
+until python generate_with_all_evidence.py --dataset "frames" --batch_size 4; do
     echo "Script crashed with exit code $?. Restarting..." >&2
     sleep 2
 done
