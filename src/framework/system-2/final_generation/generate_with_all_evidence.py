96e34681489a22f0c930589ed8bad3a77982cc55
Saptarshi <ssengupta8@gmail.com>
2026-03-11 19:44:46 -0400

    framework: frames keeps screwing up

--- src/framework/system-2/final_generation/generate_with_all_evidence.py
+++ src/framework/system-2/final_generation/generate_with_all_evidence.py
@@ -75,7 +75,7 @@ def main(model_name:str, dataset:str, batch_size: int, gpu_id: str, output_dir: 
         for batch in tqdm(torch_dataset_dataloader):
             with torch.no_grad():
                 batch_questions = main_dataset.iloc[start_idx: start_idx + batch_size]['question'].to_list()
-                generated_ids = model.generate(**batch, max_new_tokens=1200, temperature=0.9)
+                generated_ids = model.generate(**batch, max_new_tokens=2048, do_sample=False, num_beams=1)
                 decoded_generation = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
                 for ques, generation in zip(batch_questions, decoded_generation):
