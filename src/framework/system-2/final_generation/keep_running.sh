940a765ad205e18cbd700cdb6fb8606e6fca8a64
Saptarshi <ssengupta8@gmail.com>
2026-03-17 19:13:28 -0400

    framework: Updated approach as I made a mistake in implementing the framework. This will be fairer w.r.t baselines

--- src/framework/system-2/final_generation/keep_running.sh
+++ src/framework/system-2/final_generation/keep_running.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-until python generate_with_all_evidence.py --dataset "frames" --batch_size 1; do
+until python generate_with_all_evidence.py --dataset "2wikimultihopqa" --batch_size 8; do
     echo "Script crashed with exit code $?. Restarting..." >&2
     sleep 2
 done
