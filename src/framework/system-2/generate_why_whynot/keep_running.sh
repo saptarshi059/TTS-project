d8035471ca4df4cd89f6450d97344dc8983b0c75
Saptarshi <ssengupta8@gmail.com>
2026-03-22 19:42:24 -0400

    framework: system2 - why/why not gen

--- src/framework/system-2/generate_why_whynot/keep_running.sh
+++ src/framework/system-2/generate_why_whynot/keep_running.sh
@@ -5,7 +5,7 @@ DATASETS=("HuggingFaceH4/MATH-500")
 for DS in "${DATASETS[@]}"; do
     echo "Starting processing for dataset: $DS"
 
-    until python gen_why_whynot.py --dataset "$DS" --strategy "why"; do
+    until python gen_why_whynot.py --dataset "$DS" --strategy "why_not"; do
         echo "Script crashed for $DS with exit code $?. Restarting..." >&2
         sleep 2
     done
