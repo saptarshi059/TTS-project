517c1a6baf6be1c1359294865e6ddba19b0503bf
Saptarshi <ssengupta8@gmail.com>
2026-03-22 12:37:51 -0400

    framework: cot parsing + evaluate_math.py update to be standalone evaluation

--- src/baselines/Chain_of_Thought/format_generations.py
+++ src/baselines/Chain_of_Thought/format_generations.py
@@ -1,5 +1,4 @@
 from datasets import load_dataset, tqdm
-from transformers import AutoTokenizer
 from argparse import ArgumentParser
 from pathlib import Path
 import pandas as pd
@@ -9,24 +8,25 @@ sys.path.append("../../utils/")
 from all_system_prompts import COT
 
 def main(dataset: str):
-    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct")
+    responses_path = Path(f"../../../all_output/{dataset.replace('/','_')}/cot")
 
-    base_path = Path(f"../../../all_output/{dataset.replace('/','_')}/cot")
-
-    raw_responses = pd.read_json(base_path / "streamed_responses.jsonl", lines=True)
+    raw_responses = pd.read_json(responses_path / "streamed_responses.jsonl", lines=True)
     base_dataset = load_dataset(dataset, split='test').to_pandas()
 
     stripped_generations = []
     for base_row, response_row in tqdm(zip(base_dataset.itertuples(), raw_responses.itertuples())):
         message = [{"role": "system", "content": COT},
                    {"role": "user", "content": rf"Question: {base_row.problem} \n\nPlease put your final numerical or algebraic answer inside \boxed{{}}."}]
-        formatted_text = tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
+        formatted_text = ""
+        for element in message:
+            formatted_text += f"{element['role']}\n{element['content']}\n"
+        formatted_text += "assistant"
 
-        stripped_generations.append(response_row.generation.split(formatted_text)[-1].strip())
+        stripped_generations.append(response_row.generation.strip(formatted_text))
 
     print("Saving formatted generations...")
     base_dataset['stripped_generation'] = stripped_generations
-    base_dataset.to_json(base_path / "formatted_generations.jsonl", lines=True, orient='records', index=False)
+    base_dataset.to_json(responses_path / "formatted_generations.jsonl", lines=True, orient='records', index=False)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
