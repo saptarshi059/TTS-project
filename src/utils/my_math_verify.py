e299fce050f74e78feb30b0c0bcaa5ccb9b525af
Saptarshi <ssengupta8@gmail.com>
2026-03-22 16:33:55 -0400

    framework: updated evaluate_math.py to now be more flexible

--- src/utils/my_math_verify.py
+++ src/utils/my_math_verify.py
@@ -1,18 +1,24 @@
+from math_verify import parse, verify
+from argparse import ArgumentParser
+from datasets import load_dataset
+import pandas as pd
 import statistics
 
-import pandas as pd
-from datasets import load_dataset
+def main(dataset:str, op_file: str):
+    base_dataset = load_dataset(dataset, split='test').to_pandas()
+    output_file = pd.read_json(op_file, lines=True)
+    responses = []
+    for base_row, response_row in zip(base_dataset.itertuples(), output_file.itertuples()):
+        gold = parse(base_row.solution)
+        answer = parse(response_row.generation)
 
-from math_verify import parse, verify
+        responses.append(verify(gold, answer))
 
+    print(f"Accuracy: {statistics.mean(responses)*100:.2f}%")
 
-base_dataset = load_dataset("HuggingFaceH4/MATH-500", split='test').to_pandas()
-output_file = pd.read_json("../../all_output/HuggingFaceH4_MATH-500/system1_math/streamed_responses.jsonl", lines=True)
-responses = []
-for base_row, response_row in zip(base_dataset.itertuples(), output_file.itertuples()):
-    gold = parse(base_row.answer)
-    answer = parse(response_row.generation)
-
-    responses.append(verify(gold, answer))
-
-print(f"Accuracy: {statistics.mean(responses)*100:.2f}%")
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--output_file", type=str, default="../../../all_output/HuggingFaceH4_MATH-500/cot/formatted_generations.jsonl")
+    parser.add_argument("--dataset", type=str, default="HuggingFaceH4/MATH-500")
+    args = parser.parse_args()
+    main(dataset=args.dataset, op_file=args.output_file)
