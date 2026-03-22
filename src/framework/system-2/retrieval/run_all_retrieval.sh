ba1f2771c0d9df47f5d8feeac27f27a3dad3dfa7
Saptarshi <ssengupta8@gmail.com>
2026-03-22 17:00:58 -0400

    framework: prepare for s2

--- src/framework/system-2/retrieval/run_all_retrieval.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
-
-for ds in "${DATASETS[@]}";do
-  echo "-------------${ds}-------------"
-  python retrieve_docs.py --dataset "$ds"
-done
--- src/framework/system-2/triple_generation/run_all_triple_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
-
-for ds in "${DATASETS[@]}";do
-  echo "-------------${ds}-------------"
-  python triple_gen.py --dataset "$ds"
-  python parse_raw_responses.py --dataset "$ds"
-done
