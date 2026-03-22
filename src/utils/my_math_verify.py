039373e122357445a67104a467c3f5445566bb8e
Saptarshi <ssengupta8@gmail.com>
2026-03-22 15:50:17 -0400

    framework: updated evaluate_math.py to now be more flexible

--- src/utils/my_math_verify.py
+++ src/utils/my_math_verify.py
@@ -7,7 +7,7 @@ from math_verify import parse, verify
 
 
 base_dataset = load_dataset("HuggingFaceH4/MATH-500", split='test').to_pandas()
-output_file = pd.read_json("../../all_output/HuggingFaceH4_MATH-500/cot/formatted_generations.jsonl", lines=True)
+output_file = pd.read_json("../../all_output/HuggingFaceH4_MATH-500/cot/streamed_generations.jsonl", lines=True)
 responses = []
 for base_row, response_row in zip(base_dataset.itertuples(), output_file.itertuples()):
     gold = parse(base_row.answer)
