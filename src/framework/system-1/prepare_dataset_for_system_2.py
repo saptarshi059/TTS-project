136c8a8e1aed6ba42a457581d3ded409210193d4
Saptarshi <ssengupta8@gmail.com>
2026-03-24 18:33:16 -0400

    system-1 now saves prediction confidence

---
+++ src/framework/system-1/prepare_dataset_for_system_2.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+from evaluate import load
+from pathlib import Path
+from tqdm import tqdm
+import pandas as pd
+
+
+def main(dataset: str):
+    squad_metric = load("squad_v2")
+    base_path = Path(f"../../../framework_output/system1/{dataset}")
+
+    ds = pd.read_json(base_path / "parsed_responses.jsonl", lines=True)
+
+    print(f"Evaluating {dataset}...")
+    completed = []
+    for idx, row in tqdm(enumerate(ds.itertuples())):
+        prediction = [{'prediction_text': row.cleaned_ans, 'id': str(idx), 'no_answer_probability': 0.}]
+        reference = [{'answers': {'answer_start': [0], 'text': [row.answer]}, 'id': str(idx)}]
+
+        score = squad_metric.compute(predictions=prediction, references=reference)
+        if score['exact'] == 100.0:
+            completed.append(row.question)
+    print(f"Completed questions from system-1 thinking: {len(completed)} ({(len(completed)/len(ds) * 100):.2f}%)")
+
+    print("Saving datasets for system-1 complete and system-2 processing...")
+    # System-1
+    system_1_ds = ds.query("question in @completed")
+    system_1_ds.to_json(base_path / "system_1_complete.jsonl", lines=True, orient='records', index=False)
+
+    # System-2
+    system_2_ds = ds.query("question not in @completed").copy()
+    system_2_ds.rename(columns={"cleaned_ans": "system_1_guess"}, inplace=True)
+    system_2_ds.to_json(base_path / "system_2_start.jsonl", lines=True, orient='records', index=False)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="2wikimultihopqa")
+    args = parser.parse_args()
+    main(dataset=args.dataset)
--- src/framework/system-1/step1.py
+++ src/framework/system-1/step1.py
@@ -1,38 +1,50 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
 from torch.utils.data import Dataset, DataLoader
 from argparse import ArgumentParser
+from functools import partial
 from pathlib import Path
 from tqdm import tqdm
 import pandas as pd
 import os, torch
+import json
 import sys
 sys.path.append("../../utils/")
 
 from all_system_prompts import SYSTEM_1
 
-class System1Dataset(Dataset):
-    def __init__(self, tokenizer, dataset, device):
+class GenerationDataset(Dataset):
+    def __init__(self, tokenizer, dataset):
         self.tokenizer = tokenizer
-        self.dataset = dataset
-        self.device = device
-        self.samples = []
-        for row in tqdm(dataset.itertuples()):
-            self.samples.append([{"role": "system", "content": SYSTEM_1},
-                                 {"role": "user", "content": f"<input>\nQuestion: {row.question}\n</input>"}])
-        self.tokenized_samples = tokenizer.apply_chat_template(self.samples, tokenize=False, add_generation_prompt=True)
-        self.model_inputs = self.tokenizer(self.tokenized_samples, padding=True, return_tensors="pt")
+        self.questions = dataset['question'].to_list()
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.questions)
 
     def __getitem__(self, idx):
-        return {
-            "input_ids": self.model_inputs["input_ids"][idx].to(self.device),
-            "attention_mask": self.model_inputs["attention_mask"][idx].to(self.device),
-        }
+        question = self.questions[idx]
+        messages = [{"role": "system", "content": SYSTEM_1},
+                    {"role": "user", "content": f"<input>\n"
+                                                f"Question: {question}\n"
+                                                f"</input>"}]
+        formatted_text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+        return {"text": formatted_text, "question": question}
 
 
-def main(model_name:str, dataset:str, batch_size: int, gpu_id: str, output_dir: str) -> None:
+def custom_collate_fn(batch, tokenizer, device):
+    texts = [item["text"] for item in batch]
+    questions = [item["question"] for item in batch]
+
+    model_inputs = tokenizer(texts, padding=True, return_tensors="pt").to(device)
+
+    return {
+        "question": questions,
+        "input_ids": model_inputs["input_ids"],
+        "attention_mask": model_inputs["attention_mask"]
+    }
+
+
+def main(model_name:str, dataset:str, batch_size: int, gpu_id: str) -> None:
     set_seed(42)
     os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
 
@@ -42,25 +54,84 @@ def main(model_name:str, dataset:str, batch_size: int, gpu_id: str, output_dir: 
                                                  attn_implementation="flash_attention_2",
                                                  device_map="auto")
 
-    main_dataset = pd.read_json(f"../../../sampled_data/{dataset}/sampled_ds.json")[['question', 'answer']]
+    ds = pd.read_json(f"../../../sampled_data/{dataset}/sampled_ds.json", lines=True)
+
+    op_dir = Path(f"../../../framework_output/{dataset}/system1/")
+    op_dir.mkdir(parents=True, exist_ok=True)
+
+    partial_op_file = op_dir / "streamed_responses.jsonl"
+    if partial_op_file.exists():
+        try:
+            completed_df = pd.read_json(partial_op_file, lines=True)
+
+            if not completed_df.empty:
+                completed_questions = set(completed_df['question'].tolist())
+                print(f"Completed Questions: {len(completed_questions)}...")
+
+                ds = ds[~ds['question'].isin(completed_questions)]
+                print(f"Questions remaining: {len(ds)}...")
+            else:
+                print("Completed file is empty. Proceeding with all questions.")
+
+        except Exception as e:
+            print(f"Error reading partial file: {e}. Starting from scratch.")
+    else:
+        print("No existing progress found. Starting fresh.")
+
     print(f"Wrapping {dataset} with torch...")
-    torch_dataset = System1Dataset(tokenizer=tokenizer, dataset=main_dataset, device=model.device)
-    torch_dataset_dataloader = DataLoader(torch_dataset, batch_size=batch_size, shuffle=False)
+    torch_dataset = GenerationDataset(tokenizer=tokenizer, dataset=ds)
 
-    print(f"{'-'*10}Running System-1 with {model_name} on {dataset}{'-'*10}")
-    raw_responses = []
-    for batch in tqdm(torch_dataset_dataloader):
-        with torch.no_grad():
-            generated_ids = model.generate(**batch, max_new_tokens=50, do_sample=False, num_beams=1)
-            raw_responses.extend(tokenizer.batch_decode(generated_ids, skip_special_tokens=True))
+    print(f"{'-'*10}FORMATTED DATASET SAMPLE{'-'*10}\n{torch_dataset[0]['text']}\n{'-'*10}")
 
-    main_dataset["raw_responses"] = raw_responses
+    # Create a version of the function that already knows the tokenizer
+    collate_with_tokenizer = partial(custom_collate_fn, tokenizer=tokenizer, device=model.device)
+    torch_dataset_dataloader = DataLoader(torch_dataset, batch_size=batch_size, shuffle=False,
+                                          collate_fn=collate_with_tokenizer)
 
-    print("Saving results...")
-    op_dir = Path(output_dir) / f"{dataset}"
-    folder = Path(op_dir)
-    folder.mkdir(parents=True, exist_ok=True)
-    main_dataset.to_json(op_dir / "raw_responses.jsonl", lines=True, orient='records', index=False)
+    print(f"{'-'*10}Running SYSTEM-1 with {model_name} on {dataset}{'-'*10}")
+    with Path(op_dir / "streamed_responses.jsonl").open("a") as file:
+        for batch in tqdm(torch_dataset_dataloader):
+            batch_questions = batch.pop('question')
+
+            with torch.no_grad():
+                # 1. Ask for scores and the full output dict
+                outputs = model.generate(
+                    **batch,
+                    max_new_tokens=20,
+                    do_sample=False,
+                    num_beams=1,
+                    return_dict_in_generate=True,
+                    output_scores=True
+                )
+
+                generated_ids = outputs.sequences
+                # 2. Extract scores (logits). This is a tuple of length = max_new_tokens
+                # Each element is a tensor of shape (batch_size, vocab_size)
+                logits = torch.stack(outputs.scores, dim=1).cpu()
+
+                # 3. Calculate confidence (e.g., Mean Log Probability)
+                # We use log_softmax to get normalized probabilities
+                log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
+
+                # Gather the log-probs of the actual tokens generated
+                # We shift generated_ids because scores start from the first generated token
+                token_log_probs = torch.gather(
+                    log_probs,
+                    2,
+                    generated_ids[:, -logits.shape[1]:].unsqueeze(-1)
+                ).squeeze(-1)
+
+                # Average log prob per sequence as a simple confidence metric
+                confidences = token_log_probs.mean(dim=-1).tolist()
+                decoded_generation = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+            for ques, generation, conf in zip(batch_questions, decoded_generation, confidences):
+                write_obj = {
+                    'question': ques,
+                    'generation': generation,
+                    'avg_log_prob': conf  # Saving the confidence score
+                }
+                file.write(json.dumps(write_obj) + '\n')
 
 
 if __name__ == "__main__":
@@ -69,7 +140,5 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", type=str, default="2wikimultihopqa")
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--gpu_id", type=str, default="0")
-    parser.add_argument("--output_directory", type=str, default="../../../framework_output/system1/")
     args = parser.parse_args()
-    main(model_name=args.model_name, dataset=args.dataset, batch_size=args.batch_size, gpu_id=args.gpu_id,
-         output_dir=args.output_directory)
+    main(model_name=args.model_name, dataset=args.dataset, batch_size=args.batch_size, gpu_id=args.gpu_id)
