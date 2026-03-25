63ae815be2243031f0f23edb7a5b48298173b6c5
Saptarshi <ssengupta8@gmail.com>
2026-03-25 16:28:26 -0400

    system-1: I have the best prompt already lol

--- src/framework/system-2/final_generation/keep_running.sh
+++ src/framework/system-2/final_generation/keep_running.sh
@@ -11,6 +11,7 @@ for DS in "${DATASETS[@]}"; do
     done
 
     python parse_generations.py --dataset "$DS"
+    python ../../../utils/answer_scorer.py --prediction_dataset_path "../../../../framework_output/$DS/system2/final_responses.jsonl" --predicted_answer_field "final_ans" --ground_truth_field "answer"
 
     echo "Finished processing $DS successfully."
     echo "-----------------------------------"
