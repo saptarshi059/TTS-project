5d8f3f75d8dbe68c3fb50fd948947edfe022e5b4
Saptarshi <ssengupta8@gmail.com>
2026-03-25 17:19:06 -0400

    system-1: I have the best prompt already lol

--- src/framework/system-2/final_generation/keep_running.sh
+++ src/framework/system-2/final_generation/keep_running.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
+DATASETS=("2wikimultihopqa")
 
 for DS in "${DATASETS[@]}"; do
     echo "Starting processing for dataset: $DS"
 
-    until python generate_with_all_evidence.py --dataset "$DS" --batch_size 8; do
-        echo "Script crashed for $DS with exit code $?. Restarting..." >&2
-        sleep 2
-    done
 
-    python parse_generations.py --dataset "$DS"
     python ../../../utils/answer_scorer.py --prediction_dataset_path "../../../../framework_output/$DS/system2/final_responses.jsonl" --predicted_answer_field "final_ans" --ground_truth_field "answer"
 
     echo "Finished processing $DS successfully."
