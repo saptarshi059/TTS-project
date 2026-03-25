87d750c1848ad121eb25786921686b6458b9368f
Saptarshi <ssengupta8@gmail.com>
2026-03-25 14:17:25 -0400

    system-1: testing a different prompt

--- src/framework/system-1/parse_system1_raw_response.py
+++ src/framework/system-1/parse_system1_raw_response.py
@@ -22,9 +22,9 @@ def main(dataset: str):
     for row in tqdm(merged_df.itertuples()):
         cleaned_string = row.generation.split(SYSTEM_1)[-1].strip()
         match = re.search(r'Answer: ', cleaned_string , re.DOTALL | re.IGNORECASE)
-        if match:
+        try:
             cleaned_response.append(match.group(1).strip())
-        else:
+        except:
             # Trying as best as possible to get the main output
             ip_string = f"Question: {row.question}\n"
             cleaned_string = cleaned_string.split(ip_string)[1].strip()
