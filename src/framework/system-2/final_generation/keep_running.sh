35d45c36c1e515cf23206c44fa2ba300a6f56700
Saptarshi <ssengupta8@gmail.com>
2026-03-25 17:21:19 -0400

    system-1: I have the best prompt already lol

--- src/framework/system-2/final_generation/keep_running.sh
+++ src/framework/system-2/final_generation/keep_running.sh
@@ -6,7 +6,7 @@ for DS in "${DATASETS[@]}"; do
     echo "Starting processing for dataset: $DS"
 
 
-    python ../../../utils/answer_scorer.py --prediction_dataset_path "../../../../framework_output/${DS}/system2/final_responses.jsonl" --predicted_answer_field "final_ans" --ground_truth_field "answer"
+    python ../../../utils/answer_scorer.py --prediction_dataset_path "../../../../framework_output/${DS}/system2/final_response/final_responses.jsonl" --predicted_answer_field "final_ans" --ground_truth_field "answer"
 
     echo "Finished processing $DS successfully."
     echo "-----------------------------------"
