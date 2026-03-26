4b3adb33fe5307ce994c0b18f726c6ac1f16bec4
Saptarshi <ssengupta8@gmail.com>
2026-03-26 19:14:18 -0400

    not running frames anymore - its just not doing well in system2

--- src/framework/system-1/run_all_system_1.sh
+++ src/framework/system-1/run_all_system_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATASETS=("2wikimultihopqa" "hotpotqa" "musique" "frames")
+DATASETS=("2wikimultihopqa" "hotpotqa" "musique")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------Running system-1 for ${ds}-------------"
