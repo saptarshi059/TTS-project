742bea1d089da8ac9a527ffe9b7beacd2a007113
Saptarshi <ssengupta8@gmail.com>
2026-04-28 17:01:10 -0400

    Working on GraphRAG-R1: updating requirements.txt- comment out flash attention

--- src/baselines/GraphRAG-R1-main/server/fix_config.py
+++ src/baselines/GraphRAG-R1-main/server/fix_config.py
@@ -1,5 +1,7 @@
 from peft import PeftConfig
 
-config = PeftConfig.from_pretrained("~/.cache/huggingface/hub/models--yuchuanyue--GraphRAG-R1/snapshots/c03a3ddffc007b5cc1bf8c878b24445cf47edf3d/checkpoints/qwen_instruct_v2/")
+base_path = "/gpuhome/sks6765/.cache/huggingface/hub/models--yuchuanyue--GraphRAG-R1/snapshots/c03a3ddffc007b5cc1bf8c878b24445cf47edf3d/checkpoints/qwen_instruct_v2/"
+
+config = PeftConfig.from_pretrained(base_path)
 config.base_model_name_or_path = "Qwen/Qwen2.5-7B-Instruct"
-config.save_pretrained("~/.cache/huggingface/hub/models--yuchuanyue--GraphRAG-R1/snapshots/c03a3ddffc007b5cc1bf8c878b24445cf47edf3d/checkpoints/qwen_instruct_v2/")
+config.save_pretrained(base_path)
