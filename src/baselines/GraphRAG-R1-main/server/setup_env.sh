a79d3d1c51736c2f767a745931090ee6b1e0bced
Saptarshi <ssengupta8@gmail.com>
2026-04-29 13:46:18 -0400

    Working on GraphRAG-R1: creating ollama compatible version of model

--- src/baselines/GraphRAG-R1-main/server/setup_env.sh
+++ src/baselines/GraphRAG-R1-main/server/setup_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/python3.11/site-packages/nvidia/nvjitlink/lib:$CONDA_PREFIX/lib/python3.11/site-packages/nvidia/cusparse/lib:$LD_LIBRARY_PATH
+
 # HippoRAG retrieval service environment setup script
 # Usage: source setup_env.sh
 
