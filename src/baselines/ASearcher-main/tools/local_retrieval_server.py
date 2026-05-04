6224b5fbeb26d1c55885c9d0370e967bb25be450
Saptarshi <ssengupta8@gmail.com>
2026-05-04 13:27:12 -0400

    ASearcher: fixing retriever server

--- src/baselines/ASearcher-main/tools/local_retrieval_server.py
+++ src/baselines/ASearcher-main/tools/local_retrieval_server.py
@@ -41,9 +41,8 @@ def load_docs(corpus, doc_idxs):
     return results
 
 def load_model(model_path: str, use_fp16: bool = False):
-    model = SentenceTransformer(model_path,
-                                tokenizer_kwargs={"padding_side": "left"})
-
+    model = SentenceTransformer(model_path)
+    model.tokenizer.padding_side = "left"
     '''model_config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
     model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
     model.eval()
