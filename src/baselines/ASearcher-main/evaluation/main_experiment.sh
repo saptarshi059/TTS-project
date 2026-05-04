d73e06c8ba5ca1e413ff52a9e2d71a0fc390c464
Saptarshi <ssengupta8@gmail.com>
2026-05-04 13:41:26 -0400

    ASearcher: fixing retriever server

--- src/baselines/ASearcher-main/evaluation/main_experiment.sh
+++ src/baselines/ASearcher-main/evaluation/main_experiment.sh
@@ -6,7 +6,7 @@ AGENT_TYPE="asearcher"
 PROMPT_TYPE="local-rag"
 SEARCH_CLIENT_TYPE="async-search-access"
 export RAG_SERVER_ADDR_DIR="/gpuhome/sks6765/TTS-project/src/baselines/ASearcher-main/rag_server"
-export PORT=3246
+export PORT=3241
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
