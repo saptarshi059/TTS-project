37407a0e731997d06743590ec6233b857669c28a
Saptarshi <ssengupta8@gmail.com>
2026-05-05 16:04:56 -0400

    All baselines completed - trying to see if trained models can be enhanced.

--- src/framework/system-1/run_all_system_1.sh
+++ src/framework/system-1/run_all_system_1.sh
@@ -4,7 +4,7 @@ DATASETS=("2wikimultihopqa" "hotpotqa" "musique")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------Running system-1 for ${ds}-------------"
-  python step1.py --dataset "$ds"
+  python step1.py --dataset "$ds" --model_name "XXsongLALA/Qwen-2.5-7B-base-RAG-RL"
   python parse_system1_raw_response.py --dataset "$ds"
   python prepare_dataset_for_system_2.py --dataset "$ds"
   python ../../utils/answer_scorer.py --prediction_dataset_path "../../../framework_output/${ds}/system1/parsed_responses.jsonl" --predicted_answer_field "system_1_guess" --ground_truth_field "answer"
--- src/framework/system-2/triple_generation/run_all_triple_gen.sh
+++ src/framework/system-2/triple_generation/run_all_triple_gen.sh
@@ -4,6 +4,6 @@ DATASETS=("2wikimultihopqa" "hotpotqa" "musique")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------${ds}-------------"
-  python triple_gen.py --dataset "$ds"
+  python triple_gen.py --dataset "$ds" --model_name "XXsongLALA/Qwen-2.5-7B-base-RAG-RL"
   python parse_raw_responses.py --dataset "$ds"
 done
