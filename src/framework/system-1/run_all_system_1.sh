648fbee7f3b491bd8173e964f10d69bc8b7fcde1
Saptarshi <ssengupta8@gmail.com>
2026-05-07 17:38:02 -0400

    All baselines completed - trying larger model

--- src/framework/system-1/run_all_system_1.sh
+++ src/framework/system-1/run_all_system_1.sh
@@ -4,7 +4,7 @@ DATASETS=("2wikimultihopqa" "hotpotqa" "musique")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------Running system-1 for ${ds}-------------"
-  python step1.py --dataset "$ds"
+  python step1.py --dataset "$ds" --model_name "Qwen/Qwen2.5-14B-Instruct"
   python parse_system1_raw_response.py --dataset "$ds"
   python prepare_dataset_for_system_2.py --dataset "$ds"
   python ../../utils/answer_scorer.py --prediction_dataset_path "../../../framework_output/${ds}/system1/parsed_responses.jsonl" --predicted_answer_field "system_1_guess" --ground_truth_field "answer"
