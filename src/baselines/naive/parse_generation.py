19fc37dedc4c45e15182d8732b5c525b48043f19
Saptarshi <ssengupta8@gmail.com>
2026-05-09 17:05:20 -0400

    Naive RAG baseline - updating reason.py for streaming.

---
+++ src/baselines/naive/parse_generation.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import pandas as pd
+import re
+import sys
+sys.path.append("../../../utils/")
+
+from all_system_prompts import NAIVE_BASELINE
+
+
+def main(dataset: str):
+    streamed_responses = pd.read_json(f"{dataset}_streamed_responses.jsonl", lines=True)
+    main_dataset = pd.read_json(f"../../../sampled_data/{dataset}/sampled_ds.json")
+
+    print('Parsing generations...')
+    final_ans = []
+    num_no_answer = 0
+    for row in streamed_responses.itertuples():
+        split_ans = row.generation.split(NAIVE_BASELINE)[1].strip()
+        match_obj = re.search(r'<final_answer>(.*?)</final_answer>', split_ans, re.DOTALL | re.IGNORECASE)
+        if match_obj:
+            final_ans.append(match_obj.group(1).strip())
+        else:
+            num_no_answer += 1
+            final_ans.append("No answer")
+
+    print(f"Number of no_answers: {num_no_answer}...")
+
+    streamed_responses['final_ans'] = final_ans
+    final_ds = main_dataset.join(streamed_responses, on='question')[['question', 'answer', 'final_ans']]
+
+    print('Saving final dataset...')
+    final_ds.to_json(f"{dataset}_final_responses.jsonl", lines=True, orient='records', index=False)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", type=str, default='2wikimultihopqa')
+    args = parser.parse_args()
+    main(dataset=args.dataset)
