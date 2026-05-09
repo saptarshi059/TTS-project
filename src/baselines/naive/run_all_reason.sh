8455ecc0d5ed4bcc4cf31e801dd608ba6b50e909
Saptarshi <ssengupta8@gmail.com>
2026-05-09 15:17:22 -0400

    Naive RAG baseline - updating reason.py for streaming.

--- src/baselines/naive/run_all_reason.sh
+++ src/baselines/naive/run_all_reason.sh
@@ -3,6 +3,9 @@
 DATASETS=("2wikimultihopqa" "hotpotqa" "musique")
 
 for ds in "${DATASETS[@]}";do
-  echo "-------------Running retrieval for ${ds}-------------"
-  python reason.py --dataset_name "$ds"
+  echo "-------------Running reason for ${ds}-------------"
+  until python reason.py --dataset_name "$ds" --batch_size 8; do
+        echo "Script crashed for $DS with exit code $?. Restarting..." >&2
+        sleep 2
+    done
 done
