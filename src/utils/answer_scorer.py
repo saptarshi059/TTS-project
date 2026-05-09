2dcd73bd3d83578e94d7be83825a3bb0143edbc0
Saptarshi <ssengupta8@gmail.com>
2026-05-09 17:31:21 -0400

    Naive RAG baseline - updating reason.py for streaming.

--- src/utils/answer_scorer.py
+++ src/utils/answer_scorer.py
@@ -12,13 +12,21 @@ def main(prediction_dataset_path: str, predicted_answer_field: str, ground_truth
     try:
         prediction_dataset = load_dataset('json', data_files=prediction_dataset_path, split='train')
         for idx, row in tqdm(enumerate(prediction_dataset)):
-            pred = "" if row[predicted_answer_field] is None else row[predicted_answer_field]
+            raw_pred = row[predicted_answer_field]
+            if (raw_pred is None) or (raw_pred is "No Answer"):
+                pred = ""
+            else:
+                pred = row[predicted_answer_field]
             predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
             references.append({'answers': {'answer_start': [0], 'text': [row[ground_truth_field]]}, 'id': str(idx)})
     except:
         prediction_dataset = pd.read_json(prediction_dataset_path, lines=True)
         for idx, row in tqdm(enumerate(prediction_dataset.itertuples())):
-            pred = "" if getattr(row, predicted_answer_field) is None else getattr(row, predicted_answer_field)
+            raw_pred = getattr(row, predicted_answer_field)
+            if (raw_pred is None) or (raw_pred is "No Answer"):
+                pred = ""
+            else:
+                pred = getattr(row, predicted_answer_field)
             predictions.append({'prediction_text': pred, 'id': str(idx), 'no_answer_probability': 0.})
             references.append({'answers': {'answer_start': [0], 'text': [getattr(row, ground_truth_field)]}, 'id': str(idx)})
 
