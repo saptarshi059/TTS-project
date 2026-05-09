3cd824a489645de12a0f4d36617c00b61c88e00d
Saptarshi <ssengupta8@gmail.com>
2026-05-09 19:51:25 -0400

    using PruneRAG prompt for naive rag

--- src/baselines/naive/run_all_reason.sh
+++ src/baselines/naive/run_all_reason.sh
@@ -8,4 +8,7 @@ for ds in "${DATASETS[@]}";do
         echo "Script crashed for $DS with exit code $?. Restarting..." >&2
         sleep 2
     done
+
+  python parse_generation.py --dataset "$ds"
+  python ../../utils/answer_scorer.py --prediction_dataset_path "${ds}_final_responses.jsonl" --predicted_answer_field "final_ans" --ground_truth_field "answer"
 done
