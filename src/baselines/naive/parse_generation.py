4bcf31b34550e9734478df189e12c0959241f856
Saptarshi <ssengupta8@gmail.com>
2026-05-09 20:55:31 -0400

    using PruneRAG prompt for naive rag

--- src/baselines/naive/parse_generation.py
+++ src/baselines/naive/parse_generation.py
@@ -18,7 +18,7 @@ def main(dataset: str):
     num_no_answer = 0
     for row in streamed_responses.itertuples():
         split_ans = row.generation.split(NAIVE_BASELINE)[1].strip()
-        match_obj = re.search(r'so the answer is:(.*)', split_ans, re.DOTALL | re.IGNORECASE)
+        match_obj = re.search(r'<final_answer>(.*?)</final_answer>', split_ans, re.DOTALL | re.IGNORECASE)
         if match_obj:
             final_ans.append(match_obj.group(1).strip())
         else:
