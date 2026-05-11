a6121a13b0913a711e911043aca1030266013d92
Saptarshi <ssengupta8@gmail.com>
2026-05-11 16:21:24 -0400

    DeepNote Baseline

--- src/baselines/DeepNote-main/src/run_all.sh
+++ src/baselines/DeepNote-main/src/run_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python main.py --method deepnote --retrieve_top_k 5 --dataset "2wikimultihopqa" --max_step 3 --max_fail_step 2 --MaxClients 5 --model "qwen2.5-7b-instruct" --device cuda:0
-python main.py --method deepnote --retrieve_top_k 5 --dataset "hotpotqa" --max_step 3 --max_fail_step 2 --MaxClients 5 --model "qwen2.5-7b-instruct" --device cuda:0
-python main.py --method deepnote --retrieve_top_k 5 --dataset "musique" --max_step 3 --max_fail_step 2 --MaxClients 5 --model "qwen2.5-7b-instruct" --device cuda:0
+python main.py --method deepnote --retrieve_top_k 5 --dataset "2wikimultihopqa" --max_step 1 --max_fail_step 1 --model "qwen2.5-7b-instruct" --device cuda:0
+python main.py --method deepnote --retrieve_top_k 5 --dataset "hotpotqa" --max_step 1 --max_fail_step 1 --model "qwen2.5-7b-instruct" --device cuda:0
+python main.py --method deepnote --retrieve_top_k 5 --dataset "musique" --max_step 1 --max_fail_step 1 --model "qwen2.5-7b-instruct" --device cuda:0
