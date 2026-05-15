219526cc132a229b848f1f59d8714bf750f64160
Saptarshi <ssengupta8@gmail.com>
2026-05-15 16:40:40 -0400

    HONESTY IS THE BEST POLICY!

--- src/baselines/GraphAnchor-main/merge_df.py
+++ src/baselines/GraphAnchor-main/merge_df.py
@@ -14,7 +14,7 @@ file_paths = {
 for ds, fp in file_paths.items():
     print(f"Processing {ds}")
     base = pd.read_json(fp[0])
-    pred = pd.read_json(fp[1])
+    pred = pd.read_json(fp[1], lines=True)
     merged = pd.merge(base, pred, on='question', how='left')
     merged.to_json(f'output/{ds}/merged_df.jsonl', lines=True, orient='records', index=False)
 
