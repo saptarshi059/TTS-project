9d8959ec7c9389eef1d8f9d1dfde7f34783947e8
Saptarshi <ssengupta8@gmail.com>
2026-05-15 16:37:44 -0400

    HONESTY IS THE BEST POLICY!

---
+++ src/baselines/GraphAnchor-main/merge_df.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+file_paths = {
+    '2wikimultihopqa': ['../../../sampled_data/2wikimultihopqa/sampled_ds.json',
+                        'output/2wikimultihopqa/emb/GraphAnchor/qwen2.5-7b-instruct/'
+                        'topk-5__max_step-3__max_fail_step-2-20260216-20:23:52.jsonl'],
+    'hotpotqa': ['../../../sampled_data/hotpotqa/sampled_ds.json',
+                 'output/hotpotqa/emb/GraphAnchor/qwen2.5-7b-instruct/topk-5__max_step-3__'
+                 'max_fail_step-2-20260216-21:08:32.jsonl',
+                 ],
+    'musique': ['../../../sampled_data/musique/sampled_ds.json',
+                'output/musique/emb/GraphAnchor/qwen2.5-7b-instruct/topk-5__max_step-3'
+                '__max_fail_step-2-20260216-21:53:39.jsonl'
+                ]
+}
+
+for ds, fp in file_paths.items():
+    print(f"Processing {ds}")
+    base = pd.read_json(fp[0])
+    pred = pd.read_json(fp[1])
+    merged = pd.merge(base, pred, on='question', how='left')
+    merged.to_json(f'output/2wikimultihopqa/merged_df.jsonl', lines=True, orient='records', index=False)
+
