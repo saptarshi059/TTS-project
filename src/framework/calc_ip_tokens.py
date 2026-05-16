c0767dab9a8cbb3af3f5e2f8ad386053007f1bca
Saptarshi <ssengupta8@gmail.com>
2026-05-15 21:12:30 -0400

    our - calc tokens

--- src/framework/calc_ip_tokens.py
+++ src/framework/calc_ip_tokens.py
@@ -1,32 +1,15 @@
 import pandas as pd
-import re
 from transformers import AutoTokenizer
 import numpy as np
 
+datasets = ['2wikimultihopqa', 'hotpotqa', 'musique']
 
-def extract_excluding_answer(text):
-    # (?:final_)? allows it to match either 'answer' or 'final_answer'
-    pattern = r'\s*<(?:final_)?answer>.*?</(?:final_)?answer>\s*'
+all_texts = []
+for ds in datasets:
+    ds_df = pd.read_json(f"../../../../framework_output/{ds}/system2/retrieval_results/with_retrieved_docs.jsonl", lines=True)
+    for row in ds_df.itertuples():
+        all_texts.extend(row.retrieved_docs)
 
-    cleaned_text = re.sub(pattern, '', text, flags=re.DOTALL)
-    return cleaned_text.strip()
-
-wiki_triples = pd.read_json('../../experiment_runs/main_framework_run/2wikimultihopqa/system2/triple_extraction/parsed_responses.jsonl', lines=True)['generated_triples'].tolist()
-wiki_triples_flat = [item for sublist in wiki_triples for item in sublist]
-wiki_responses = pd.read_json('../../experiment_runs/main_framework_run/2wikimultihopqa/system2/final_response/streamed_responses.jsonl', lines=True)['generation'].tolist()
-wiki_responses_cleaned = [extract_excluding_answer(x) for x in wiki_responses]
-
-hopotqa_triples = pd.read_json('../../experiment_runs/main_framework_run/hotpotqa/system2/triple_extraction/parsed_responses.jsonl', lines=True)['generated_triples'].tolist()
-hopotqa_triples_flat = [item for sublist in hopotqa_triples for item in sublist]
-hopotqa_responses = pd.read_json('../../experiment_runs/main_framework_run/hotpotqa/system2/final_response/streamed_responses.jsonl', lines=True)['generation'].tolist()
-hopotqa_responses_cleaned = [extract_excluding_answer(x) for x in hopotqa_responses]
-
-musique_triples = pd.read_json('../../experiment_runs/main_framework_run/musique/system2/triple_extraction/parsed_responses.jsonl', lines=True)['generated_triples'].tolist()
-musique_triples_flat = [item for sublist in musique_triples for item in sublist]
-musique_responses = pd.read_json('../../experiment_runs/main_framework_run/musique/system2/final_response/streamed_responses.jsonl', lines=True)['generation'].tolist()
-musique_responses_cleaned = [extract_excluding_answer(x) for x in musique_responses]
-
-all_texts = wiki_triples_flat + wiki_responses_cleaned + hopotqa_triples_flat + hopotqa_responses_cleaned + musique_triples_flat + musique_responses_cleaned
 tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-7B-Instruct')
 all_reasoning = tokenizer(all_texts)
 
