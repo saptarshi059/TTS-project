e3b804e55af53a222f338711206768e2a92f4a92
Saptarshi <ssengupta8@gmail.com>
2026-07-08 14:41:47 -0400

    testing different models for rebuttal

--- src/framework/system-1/run_all_system_1.sh
+++ src/framework/system-1/run_all_system_1.sh
@@ -4,8 +4,8 @@ DATASETS=("2wikimultihopqa" "musique" "hotpotqa")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------Running system-1 for ${ds}-------------"
-  python step1.py --dataset "$ds"
-  python parse_system1_raw_response.py --dataset "$ds" --model_name "meta-llama/Llama-3.1-8B"
+  python step1.py --dataset "$ds"  --model_name "meta-llama/Llama-3.1-8B"
+  python parse_system1_raw_response.py --dataset "$ds"
   python prepare_dataset_for_system_2.py --dataset "$ds"
   python ../../utils/answer_scorer.py --prediction_dataset_path "../../../framework_output/${ds}/system1/parsed_responses.jsonl" --predicted_answer_field "system_1_guess" --ground_truth_field "gold_answer"
 done
