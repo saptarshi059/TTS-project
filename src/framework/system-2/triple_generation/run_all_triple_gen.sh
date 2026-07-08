2f38c5b225908181ef07e2ec9857ca26db2e700e
Saptarshi <ssengupta8@gmail.com>
2026-07-08 14:40:03 -0400

    testing different models for rebuttal

--- src/framework/system-2/triple_generation/run_all_triple_gen.sh
+++ src/framework/system-2/triple_generation/run_all_triple_gen.sh
@@ -4,6 +4,6 @@ DATASETS=("2wikimultihopqa" "musique" "hotpotqa")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------${ds}-------------"
-  python triple_gen.py --dataset "$ds"
+  python triple_gen.py --dataset "$ds"  --model_name "meta-llama/Llama-3.1-8B"
   python parse_raw_responses.py --dataset "$ds"
 done
