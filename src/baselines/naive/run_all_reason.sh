9534a63094c02163d034f952088ad40981b1e27b
saptarshi059 <45448695+saptarshi059@users.noreply.github.com>
2026-09-09 14:42:17 -0400

    Update run_all_reason.sh

--- src/baselines/naive/run_all_reason.sh
+++ src/baselines/naive/run_all_reason.sh
@@ -4,7 +4,7 @@ DATASETS=("2wikimultihopqa" "hotpotqa" "musique")
 
 for ds in "${DATASETS[@]}";do
   echo "-------------Running reason for ${ds}-------------"
-  until python reason.py --dataset_name "$ds" --batch_size 8 --model_name "meta-llama/Llama-3.1-8B-Instruct"; do
+  until python reason.py --dataset_name "$ds" --batch_size 8 --model_name "allenai/Olmo-3-7B-Instruct"; do
         echo "Script crashed for $DS with exit code $?. Restarting..." >&2
         sleep 2
     done
