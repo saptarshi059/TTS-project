a224d02e5aa797a0f6b125a1fff9c41150f51f52
saptarshisengupta <ssengupta8@gmail.com>
2026-09-12 16:40:46 -0400

    PAGER - olmo

--- src/baselines/PAGER-main/src/construct_outline.py
+++ src/baselines/PAGER-main/src/construct_outline.py
@@ -225,7 +225,7 @@ def main():
         tensor_parallel_size=len(args.gpu_ids.split(",")),
         trust_remote_code=True,
         seed=args.seed,
-        gpu_memory_utilization=0.4
+        gpu_memory_utilization=0.3
     )
     tokenizer = AutoTokenizer.from_pretrained(
         args.model_name,
